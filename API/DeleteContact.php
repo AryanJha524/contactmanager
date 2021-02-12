a175aabd02bb93240df74483a68c0aafc2096d9e
Aryan Jha <aryanjha@gmail.com>
2021-02-12 15:33:05 -0500

    finished for testing, need to still work on validating user

--- API/DeleteContact.php
+++ API/DeleteContact.php
@@ -1,55 +1,60 @@
 <?php
+	
     $inData = getRequestInfo();
 	$contactId = $inData["id"];
 	$userId = $inData["userId"];
 
-	$servername = "104.131.122.53";
-	$username = "root";
-	$password = "cop4331g5mysql";
+	$servername = "localhost";
+	$username = "Aryan";
+	$password = "testing321";
 	$dbname = "COP4331";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 	if ($conn->connect_error) 
 	{
-		returnWithError( $conn->connect_error );
+		returnWithError($conn->connect_error);
 	} 
 	else
 	{
-		if (validateUser($contactId, $userId) != true)
+		// have to fix this function right now
+		/*if (validateUser($contactId, $userId))
 		{
 			returnWithError("User not authorized to delete contact");
 		}
 		else
-		{
-			$sql = "DELETE FROM Contacts WHERE id=?";
-			$stmt = $conn->prepare($sql);
+		{*/
+			$sql = "DELETE FROM COP4331.Contacts WHERE id=$contactId";
+			/*$stmt = $conn->prepare($sql);
 			$stmt->bind_param("i", $contactId);
 			$stmt->execute();
-
-			$result = $stmt->get_result();
+			$result = $stmt->get_result(); */
+			
 			if( $result = $conn->query($sql) != TRUE )
 			{
-				returnWithError( $conn->error );
+				returnWithError( "Couldn't successfully delete contact" );
 			}
-			returnWithSuccess("Contact successfully deleted!");
+			else
+			{
+				returnWithSuccess("Contact successfully deleted!");
+			}
 			$conn->close();
-		}
+		// }
 	}
 
 	function validateUser($contactId, $userId)
     {
-		$conn = new mysqli($servername, $username, $password, $dbname);
+		$conn = new mysqli("localhost", "Aryan", "testing321", "COP4331");
 		if ($conn->connect_error) 
 		{
-			returnWithError( $conn->connect_error );
+			returnWithError( "Couldn't connect to database to validate user" );
 		}
 		else
 		{
 			// create an sql query that retrieves the foreign key of this contact
-			$sql = "SELECT userId FROM Contacts where id='" . $contactId . "'";
+			$sql = "SELECT UserId FROM COP4331.Contacts where ID=".$contactId;
 			if ($result = $conn->query($sql) != TRUE)
 			{
-				returnWithError( $conn->error );
+				returnWithError( "Couldn't find user to validate" );
 			}
 			else
 			{
@@ -62,6 +67,10 @@
 					{
 						$conn->close();
 						return TRUE;
+					}
+					else
+					{
+						return FALSE;
 					}
 				}
 			}
